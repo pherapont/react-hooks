99fec1787cd76741f2944b844702d695d69b0d8f
pherapont <pherapont@yandex.ru>
2020-10-01 06:43:48 +0700

    Работа с useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import './App.css'
 
 export default function App() {
 
 	const [ type, setType ] = useState( 'users' )
+	const [ data, setData ] = useState ( [] )
+
+	console.log(data)
+
+	// useEffect(() => {
+	// 	console.log('render')
+	// })
+
+	useEffect(() => {
+		fetch(`https://jsonplaceholder.typicode.com/` + type)
+		.then(response => response.json())
+		.then(json => setData(json))
+	}, [type])
 
 	return (
 		<div className="App mt-md-5">
@@ -13,6 +26,8 @@ export default function App() {
 			<button onClick={() => setType( 'users' )} className="btn btn-info mr-md-3">Пользователи</button>
 			<button onClick={() => setType( 'todos' )} className="btn btn-success mr-md-3">Todo</button>
 			<button onClick={() => setType( 'posts' )} className="btn btn-danger">Посты</button>
+
+			<pre>{JSON.stringify(data, null, 2)}</pre>
 		</div>
 	)
 }
