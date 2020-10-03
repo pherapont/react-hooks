056066b7b8aa8d6599bc85433d5d90f3f70ec30f
pherapont <pherapont@yandex.ru>
2020-10-03 07:10:03 +0700

    Исправление ошибки путем вынесения переменной за пределы компонента (что не правильно) [A

--- src/App.js
+++ src/App.js
@@ -2,17 +2,27 @@ import React, { useState, useEffect } from 'react'
 
 import './App.css'
 
-export default function App() {
-	
-	// Следующие 4 строки загоняют программу в бесконченый цикл
-	const [renderCounter, setRenderCounter] = useState(1)
+let renderCounter = 1
+
+function App() {
+	const [inputValue, setInputValue] = useState('initial')
+
 	useEffect(() => {
-		setRenderCounter(prev => prev + 1)
-	}) 
+		renderCounter++
+	})
+
+	// Следующие 4 строки загоняют программу в бесконченый цикл
+	// const [renderCounter, setRenderCounter] = useState(1)
+	// useEffect(() => {
+	// 	setRenderCounter(prev => prev + 1)
+	// }) 
 
 	return (
 		<div>
 			<h1>Страница рендерилась {renderCounter} раз</h1>
+			<input value={inputValue} onChange={e => setInputValue(e.target.value)} />
 		</div>
 	)
 }
+
+export default App
